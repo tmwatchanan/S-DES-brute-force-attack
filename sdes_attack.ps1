aa0fb64d3e47c3a703de84e9be1868faae24adc6
tmwatchanan <tmwatchanan@gmail.com>
2017-09-26 01:44:58 +0700

    Refactor code and make output file writing

--- sdes_attack.ps1
+++ sdes_attack.ps1
@@ -6,7 +6,8 @@ $ciphertext = Get-Content ciphertext.txt -First 1
 $ciphertext = $ciphertext.TrimEnd(',')
 "ciphertext = {0}" -f $ciphertext
 
-$numStudentCode = 9
+$studentCode = '570610601'
+$numStudentCode = $studentCode.Length
 $ciphertext_array = $ciphertext.Split(",")
 $ciphertext_array = $ciphertext_array.TrimStart("0b")
 $ciphertext_array = $ciphertext_array.PadLeft(8,'0')
@@ -15,7 +16,7 @@ $known_ciphertext = $ciphertext_array[0..($numStudentCode-1)]
 "> starting brute force SDES  ........."
 
 # Loop: Key
-For ($key = 0; $key -lt 1024; $key += 1)
+For ($key = 350; $key -lt 1024; $key += 1)
 {
     $key_binary = [Convert]::ToString($key,2).PadLeft(10,'0')
     "key={0} [{1}]" -f $key, $key_binary
@@ -27,7 +28,7 @@ For ($key = 0; $key -lt 1024; $key += 1)
         $output[$i] = .\Simplified-DES-master\sdes.exe decrypt $known_ciphertext[$i] $key_binary
         #"{0} -> {1}" -f $known_ciphertext[$i], $output[$i]
     }
-    if (($output -join "") -match '570610601')
+    if (($output -join "") -match $studentCode)
     {
         "Found! key used for this cipertext is {0} [{1}]" -f $key, $key_binary
         $plaintext = @()
@@ -36,7 +37,9 @@ For ($key = 0; $key -lt 1024; $key += 1)
             $plaintext_block = .\Simplified-DES-master\sdes.exe decrypt $block $key_binary
             $plaintext += $plaintext_block
         }
-        "plaintext = {0}" -f [string]$plaintext -replace ' ', ''
+        $plaintext = [string]$plaintext -replace ' ', ''
+        $plaintext | out-file "plaintext.txt"
+        "plaintext = {0}" -f $plaintext
         break
     }
 }
